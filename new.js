1236191e0b1b5270db51a6cfa9346028bf52952e
Trevor Lohrding <trevorlohrding@gmail.com>
2021-06-18 17:29:39 -0500

    Add new.js for low ram start

---
+++ new.js
@@ -0,0 +1,27 @@
+export async function main(ns) {
+    let reason = ns.args[0];
+    const processes = ns.ps("home");
+    for (let i = 0; i < processes.length; ++i) {
+        if (processes[i].filename !== "Master.js") {
+            ns.scriptKill(processes[i].filename, "home")
+        }
+    }
+    await ns.run("scanner.js", 1);
+    await ns.sleep(8000);
+    let homeRamInfo = ns.getServerRam("home");
+    let totalRam = homeRamInfo[0];
+    let targets = ns.read("targets.txt").split(",");
+    let ramLeft = totalRam - homeRamInfo[1];
+    let threads = Math.floor(
+        Math.floor((ramLeft * 0.8) / targets.length) /
+        ns.getScriptRam("node-hack.js", "home")
+    );
+    for (let target of targets) {
+        ns.tprint(target);
+        await runHack(ns, "node-hack.js", "home", threads, target);
+        await ns.sleep(2000);
+    }
+    if(ns.getServerRam("home")[0] - ns.getServerRam("home")[0] > ns.getScriptRam("stockmarket.js", "home")){
+        await ns.run("stockmarket.js", 1);
+    }
+}
