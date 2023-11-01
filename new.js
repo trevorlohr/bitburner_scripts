a8aeb5e14b0aa02be63fee66b5292bcaada5841b
trevorlohr <25940646+trevorlohr@users.noreply.github.com>
2023-11-01 16:38:37 -0500

    Update new.js
    Adjusted for changes in deprecated functions

--- new.js
+++ new.js
@@ -3,10 +3,11 @@ import { runHack } from "hackutility.js";
 export async function main(ns) {
     await ns.run("scanner.js", 1);
     await ns.sleep(8000);
-    let homeRamInfo = ns.getServerRam("home");
-    let totalRam = homeRamInfo[0];
+    let homeRamInfo = ns.getServerMaxRam("home");
+    let totalRam = homeRamInfo;
+    let usedRam = ns.getServerUsedRam("home")
     let targets = ns.read("targets.txt").split(",");
-    let ramLeft = totalRam - homeRamInfo[1];
+    let ramLeft = totalRam - usedRam;
     let threads = Math.floor(
         Math.floor((ramLeft * 0.8) / targets.length) /
         ns.getScriptRam("node-hack.js", "home")
@@ -16,7 +17,7 @@ export async function main(ns) {
         await runHack(ns, "node-hack.js", "home", threads, target);
         await ns.sleep(2000);
     }
-    if(ns.getServerRam("home")[0] - ns.getServerRam("home")[0] > ns.getScriptRam("stockmarket.js", "home")){
+    if(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") > ns.getScriptRam("stockmarket.js", "home")){
         await ns.run("stockmarket.js", 1);
     }
 }
