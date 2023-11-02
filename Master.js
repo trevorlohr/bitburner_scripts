0f1aca9189a21beca015576f6e2cbfcb076019e3
trevorlohr <25940646+trevorlohr@users.noreply.github.com>
2023-11-02 15:47:27 -0500

    Update Master.js

--- Master.js
+++ Master.js
@@ -40,8 +40,8 @@
         }
         await ns.run("scanner.js", 1);
         await ns.sleep(8000);
-        let homeRamInfo = ns.getServerRam("home");
-        let totalRam = homeRamInfo[0];
+        let homeRamInfo = ns.getServerMaxRam("home");
+        let totalRam = homeRamInfo;
 
 
         if (reason === "nodes") {
@@ -82,7 +82,7 @@
             await runHack(ns, "node-hack.js", "home", threads, target);
             await ns.sleep(2000);
         }
-        if(ns.getServerRam("home")[0] - ns.getServerRam("home")[0] > ns.getScriptRam("stockmarket.js", "home")){
+        if(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") > ns.getScriptRam("stockmarket.js", "home")){
             await ns.run("stockmarket.js", 1);
         }
         }
